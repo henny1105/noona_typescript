3fa1c488a2a17557decc52467ec01ca4296597e8
kimhyein <70068498+henny1105@users.noreply.github.com>
2025-06-03 00:55:27 +0900

    feat: page 생성 및 router 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router';
+import AppLayout from './layout/AppLayout';
+import HomePage from './pages/HomePage/HomePage';
+import SearchPage from './pages/SearchPage/SearchPage';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Routes>
+			<Route path='/' element={<AppLayout />}>
+				<Route index element={<HomePage />} />
+				<Route path='search' element={<SearchPage />} />
+				{/* <Route path='search/:keyword' element={<SearchWithKeywordPage />} /> */}
+				{/* <Route path='playlist/:id' element={<PlaylistDetailPage />} /> */}
+				{/* <Route path="playlist" element={<PlaylistPage />} /> */}
+			</Route>
+		</Routes>
 	);
 }
 
