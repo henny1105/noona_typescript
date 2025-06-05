f4f6be07b25affc697b329d606535d23db7a9231
kimhyein <70068498+henny1105@users.noreply.github.com>
2025-06-06 01:25:14 +0900

    feat: NewRelease 파일 추가

---
+++ src/pages/HomePage/components/NewRelease.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const NewRelease = () => {
+	return (
+		<div>
+			<Typography variant='h1' paddingTop='8px'>
+				New Release
+			</Typography>
+		</div>
+	);
+};
+
+export default NewRelease;
