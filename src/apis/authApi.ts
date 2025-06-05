f9db80146babb36c6ba0ba229777b52444c8c670
kimhyein <70068498+henny1105@users.noreply.github.com>
2025-06-06 03:18:09 +0900

    feat: 스포티파이 토큰 받기

---
+++ src/apis/authApi.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { URLSearchParams } from 'url';
+import { clientId, clientSecret } from '../configs/authConfig';
+
+const encodedBase64 = (data: string) => {
+	return Buffer.from(data).toString('base64');
+};
+
+export const getClientCredentialToken = async () => {
+	try {
+		const body = new URLSearchParams({
+			grant_type: 'client_credentials',
+		});
+
+		const response = await axios.post('https://accounts.spotify.com/api/token', body, {
+			headers: {
+				'Authorization': `Basic ${encodedBase64(clientId + ':' + clientSecret)}`,
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+		});
+		return response.data;
+	} catch (error) {
+		throw new Error('Failed to get client credential token');
+	}
+};
